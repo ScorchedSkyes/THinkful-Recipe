f415caa4cc086c50ed9841e65db2e344cc0f1421
ScorchedSkyes <Kris.harder1929@gmail.com>
2022-11-21 08:19:10 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData";
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  const addRecipe = (data) => {
+    setRecipes([...recipes, data]);
+  };
+
+  function deleteRecipe(targetIndex) {
+    setRecipes(recipes.filter((_, index) => index !== targetIndex));
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Delicious Food Recipes</h1>
+      </header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate addRecipe={addRecipe} />
+    </div>
+  );
+}
+
+export default App;
